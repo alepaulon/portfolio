4332a0341465cc0e81119bf5f59d48bea84b7f93
Alejo Paulon <alejo.paulon@gmail.com>
2023-06-12 09:17:09 -0300

    more small changes

--- app/about/page.js
+++ app/about/page.js
@@ -10,7 +10,7 @@ export default function About() {
                         <h2 className='pt-4'>My name is <b>Alejo Paulón</b>, I'm a 24 years old guy from Argentina.</h2>
                         <h2 className='pt-4'>I've been interested in the tech world since my childhood, but I got fully involved into learning Web Development during the pandemic, early 2021.</h2>
                         <h2 className='pt-4'>As of now, my stack is <b>Javascript, React, NextJS & TailwindCSS.</b> With plenty more to discover about this world, I'd like to get a job to apply my knowledge and keep learning new things.</h2>
-                        <h2 className='pt-4'>Wanna talk with me? Be free to contact me on my<Link href='https://www.linkedin.com/in/alejopaulon/' target="_blank" className="font-semibold"> LinkedIn</Link>.</h2>
+                        <h2 className='pt-4'>Wanna talk with me? Feel free to contact me on my<Link href='https://www.linkedin.com/in/alejopaulon/' target="_blank" className="font-semibold"> LinkedIn</Link>.</h2>
                     </div>
                 </span>
             </div>
